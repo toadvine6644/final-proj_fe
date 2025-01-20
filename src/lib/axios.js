b62946ecf19edf3be9d01e87948f7870703c0be6
toadvine6644 <tikkboi@gmail.com>
2025-01-20 18:33:18 +0700

    deploy

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,11 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstancs = axios.create({
-    baseURL: import.meta.mode === "development" ? "http://localhost:5000/api" : "/api",
-    withCredentials: true,
-})
+  baseURL:
+    import.meta.mode === "development"
+      ? "https://final-projbe-production.up.railway.app/api"
+      : "/api",
+  withCredentials: true,
+});
 
 export default axiosInstancs;
